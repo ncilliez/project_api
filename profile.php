82274b1b614270b64424882096b663fd6f1f04dd
Nicolas Cilliez <ncilliez@gmail.com>
2023-08-22 18:54:04 +0200

    profile.php

---
+++ profile.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once("db_connect.php");
+$request_method = $_SERVER["REQUEST_METHOD"];
+
+switch ($request_method) {
+    case 'GET':     
+        getProfile();
+    break;
+
+    case 'POST':
+       
+    break;
+    
+    default:
+        header("HTTP/1.0 405 Method Not Allowed");
+    break;
+}
+
+function getProfile(){
+    $conn = getConnexion();
+
+    $query = "SELECT p.id, p.nom, p.naissance, p.adresse, p.mail, p.lien, p.commentaire, f.path_fichier 
+              FROM profile p INNER JOIN fichier f ON p.id_fichier = f.id";
+    $stmt = $conn->prepare($query);
+    $stmt->execute();
+    $response = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $stmt->closeCursor();
+
+    sendJSON($response);
+}
+
+function sendJSON($result){
+    header("Access-Control-Allow-origin: *");
+    header("Content-Type: application/json; charset= UTF-8");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+    header("Access-Control-Max-Age: 3600");
+    header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, X-Token-Auth, Authorization");
+    header("Access-Control-Allow-Credentials: true");
+    echo json_encode($result, JSON_UNESCAPED_UNICODE);
+}
+
+?>
